8a0b3c75f296a2691b79e9a7a3bfd4ee6a000a64
UWINGENEYE-Claudine <cuwi2020@gmail.com>
2018-12-21 16:49:17 +0200

    create variable

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+       
+var q1=input.radio["myFunction"]["form-control"].value;
+var q2=input.radio["myFUnction"]["form-control"].value;
+var q3=input.radio["myFunction"]["form-control"].value;
+var q4=input.radio["myFunction"]["form-control"].value;
+
+var quetion1=[q1, q2,q3,q4];
+
+if(q1=="concat")
+{
+ marks=marks+5;
+ }
+if(q2=="split")
+{
+ marks=marks+5;
+ }
+if(q3=="valueOf")
+{
+marks=marks+5;
+}
+if(q4=="Storing numbers,dates or other values")
+{
+ marks=marks+5;
+}
+
+});
